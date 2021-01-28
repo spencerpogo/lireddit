f36897803f21f2373368de432c93cec27593f68c
Scoder12 <34356756+Scoder12@users.noreply.github.com>
2021-01-27 17:27:37 -0800

    Feat: Query post by ID

--- server/src/resolvers/post.ts
+++ server/src/resolvers/post.ts
@@ -1,11 +1,19 @@
 import { MyContext } from "src/types";
-import { Ctx, Query, Resolver } from "type-graphql";
+import { Arg, Ctx, Int, Query, Resolver } from "type-graphql";
 import { Post } from "../entities/Post";
 
 @Resolver()
 export class PostResolver {
   @Query(() => [Post])
-  posts(@Ctx() { em }: MyContext) {
+  posts(@Ctx() { em }: MyContext): Promise<Post[]> {
     return em.find(Post, {});
+  }
+
+  @Query(() => Post, { nullable: true })
+  post(
+    @Arg("id", () => Int) id: number,
+    @Ctx() { em }: MyContext
+  ): Promise<Post | null> {
+    return em.findOne(Post, { id });
   }
 }
