e2af1d7b84cb0f4dbc450e88d258f2adaeb90315
Scoder12 <34356756+Scoder12@users.noreply.github.com>
2021-01-28 15:28:27 -0800

    Feat: Length validation in register

--- server/src/resolvers/user.ts
+++ server/src/resolvers/user.ts
@@ -53,19 +53,51 @@ export class UserResponse {
 
 @Resolver()
 export class UserResolver {
-  @Mutation(() => User)
+  @Mutation(() => UserResponse)
   async register(
     @Arg("options")
     { username, password }: UsernamePasswordInput,
     @Ctx() { em }: MyContext
-  ): Promise<User> {
+  ): Promise<UserResponse> {
+    if (username.length <= 2) {
+      return {
+        errors: [
+          {
+            field: "username",
+            message: "Username must be at least 3 characters",
+          },
+        ],
+      };
+    }
+    if (username.length >= 50) {
+      return {
+        errors: [
+          {
+            field: "username",
+            message: "Username must be shorter than 50 characters",
+          },
+        ],
+      };
+    }
+
+    if (password.length <= 3) {
+      return {
+        errors: [
+          {
+            field: "password",
+            message: "Password must be at least 4 characters",
+          },
+        ],
+      };
+    }
+
     const hashedPassword = await argon2.hash(password, argon2Config);
     const user = await em.create(User, {
       username,
       password: hashedPassword,
     });
     await em.persistAndFlush(user);
-    return user;
+    return { user };
   }
 
   @Mutation(() => UserResponse)
