749f68fec0a340859ef667ce14d406900323057e
Scoder12 <34356756+Scoder12@users.noreply.github.com>
2021-01-28 17:19:52 -0800

    Setup express session

--- server/src/index.ts
+++ server/src/index.ts
@@ -1,6 +1,9 @@
 import { MikroORM } from "@mikro-orm/core";
 import { ApolloServer } from "apollo-server-express";
+import connectRedis from "connect-redis";
 import express from "express";
+import session from "express-session";
+import redis from "redis";
 import { buildSchema } from "type-graphql";
 import { __PROD__ } from "./constants";
 import MIKRO_CONFIG from "./mikro-orm.config";
@@ -10,10 +13,37 @@ import { UserResolver } from "./resolvers/user";
 import { MyContext } from "./types";
 
 async function main() {
+  const SESSION_SECRET = process.env.LIREDDIT_SESSION_SECRET;
+  if (!SESSION_SECRET) {
+    throw new Error("Missing LIREDDIT_SESSION_SECRET environment variable");
+  }
+
   const orm = await MikroORM.init(MIKRO_CONFIG);
   await orm.getMigrator().up();
 
   const app = express();
+
+  const RedisStore = connectRedis(session);
+  const redisClient = redis.createClient();
+
+  app.use(
+    session({
+      name: "qid",
+      store: new RedisStore({
+        client: redisClient,
+        disableTouch: true,
+        disableTTL: true,
+      }),
+      cookie: {
+        maxAge: 1000 * 60 * 60 * 24 * 365 * 10, // 10 years
+        httpOnly: true,
+        sameSite: "lax", // CSRF
+        secure: __PROD__, // Should cookie only work over HTTPS
+      },
+      secret: SESSION_SECRET,
+      resave: false,
+    })
+  );
 
   const schema = await buildSchema({
     resolvers: [HelloResolver, PostResolver, UserResolver],
