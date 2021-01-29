cd8cbb07fb308b469e1bc93bd75a57a289f48c21
Scoder12 <34356756+Scoder12@users.noreply.github.com>
2021-01-28 20:44:40 -0800

    Set up URQL provider

--- web/src/pages/_app.tsx
+++ web/src/pages/_app.tsx
@@ -1,17 +1,24 @@
 import { ChakraProvider, ColorModeProvider } from "@chakra-ui/react";
 import { AppProps } from "next/dist/next-server/lib/router/router";
+import { createClient, Provider } from "urql";
+
+// TODO: Make this configurable for prod
+const client = createClient({ url: "http://localhost:4000/graphql" });
 
 function MyApp({ Component, pageProps }: AppProps) {
+  // TODO: Refactor to not use provider here
   return (
-    <ChakraProvider resetCSS>
-      <ColorModeProvider
-        options={{
-          useSystemColorMode: true,
-        }}
-      >
-        <Component {...pageProps} />
-      </ColorModeProvider>
-    </ChakraProvider>
+    <Provider value={client}>
+      <ChakraProvider resetCSS>
+        <ColorModeProvider
+          options={{
+            useSystemColorMode: true,
+          }}
+        >
+          <Component {...pageProps} />
+        </ColorModeProvider>
+      </ChakraProvider>
+    </Provider>
   );
 }
 
