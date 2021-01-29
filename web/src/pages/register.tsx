dcf05fce4685f45c8f4b6f8fffe1367655dd9408
Scoder12 <34356756+Scoder12@users.noreply.github.com>
2021-01-28 19:43:21 -0800

    Add register page

---
+++ web/src/pages/register.tsx
@@ -0,0 +1,9 @@
+import { FC } from "react";
+
+export interface RegisterProps {}
+
+export const Register: FC<RegisterProps> = ({}: RegisterProps) => {
+  return <div>Register page</div>;
+};
+
+export default Register;
