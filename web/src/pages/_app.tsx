bc453f8836895b4a94a378df1ecb6b08df3e7107
Scoder12 <34356756+Scoder12@users.noreply.github.com>
2021-01-29 18:08:55 -0800

    Include credentials in graphql requests

--- web/src/pages/_app.tsx
+++ web/src/pages/_app.tsx
@@ -3,7 +3,10 @@ import { AppProps } from "next/dist/next-server/lib/router/router";
 import { createClient, Provider } from "urql";
 
 // TODO: Make this configurable for prod
-const client = createClient({ url: "http://localhost:4000/graphql" });
+const client = createClient({
+  url: "http://localhost:4000/graphql",
+  fetchOptions: { credentials: "include" },
+});
 
 function MyApp({ Component, pageProps }: AppProps) {
   // TODO: Refactor to not use provider here
