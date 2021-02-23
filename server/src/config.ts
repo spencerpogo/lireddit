f915a9c0104d6a0005e67c214a5d913cac0d50fc
Scoder12 <34356756+Scoder12@users.noreply.github.com>
2021-02-23 11:07:40 -0800

    Create config.ts
    Should have been in the last commit

---
+++ server/src/config.ts
@@ -0,0 +1,33 @@
+import { ENV_PREFIX } from "./constants";
+
+function getenv(key: string): string {
+  const val = process.env[ENV_PREFIX + key];
+  if (!val) {
+    throw new Error(`Missing required environment variable: ${key}`);
+  }
+  return val;
+}
+
+function expectNum(val: string): number {
+  const n = Number(val);
+  if (isNaN(n)) {
+    throw new Error(`Environment variable ${n} should be a number`);
+  }
+  return n;
+}
+
+export const CONFIG = {
+  port: expectNum(getenv("PORT")),
+  iface: getenv("IFACE"),
+  frontendUrl: getenv("FRONTEND_URL"),
+  sessionSecret: getenv("SESSION_SECRET"),
+  db: {
+    host: getenv("DB_HOST"),
+    port: expectNum(getenv("DB_PORT")),
+    dbName: getenv("DB_DBNAME"),
+    user: getenv("DB_USER"),
+    password: getenv("DB_PWD"),
+  },
+};
+
+export default CONFIG;
