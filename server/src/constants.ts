b0c224ce96af8d98712234bdf96577105d9b7d0a
Scoder12 <34356756+Scoder12@users.noreply.github.com>
2021-02-27 12:01:16 -0800

    Add reset password constants

--- server/src/constants.ts
+++ server/src/constants.ts
@@ -1,6 +1,8 @@
+export const SITE_NAME = "Lireddit";
 export const __PROD__ = process.env.NODE_ENV === "production";
 export const ENV_PREFIX = "LIREDDIT_";
 export const COOKIE_NAME = "qid";
+export const FORGOT_PASSWORD_FRONTEND_ROUTE = "/reset-password/";
 export const FORGOT_PASSWORD_REDIS_PREFIX = "forgot-password:";
 export const FORGOT_PASSWORD_VALID_TIME_MS = 1000 * 60 * 60 * 3; // 3 hours
 export const FORGOT_PASSWORD_VALID_TIME_HUMAN_READABLE = "3 hours";
